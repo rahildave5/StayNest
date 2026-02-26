161ad0951821e9d8f2e1d49d3c09dcd9de2dc3cd
Rahil Dave <153646627+rahildave5@users.noreply.github.com>
2026-02-26 19:34:25 +0100

    Implement CRUD operations for listings
    Added CRUD routes for listings and middleware setup.

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+//REQUIRED PACKAGES
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
+//CONNECTING TO DB
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/bookBNB   ")
 }
@@ -18,17 +22,25 @@ main()
         }
     )
 
+//SEEDING THE DB
 app.listen(5050, () => {
     console.log("server is running on port");
 })
 
+//MIDDLEWARE
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));    
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 
+//ROUTES
 app.get("/", (req, res) => {
     res.send("server connected");
 })
 
+//TESTING ROUTE to check if the connection to DB is working and if we can save a sample listing to the DB
 app.get("/testing", async(req, res) => {
     let sampleListing = new Listing(
         {
@@ -44,8 +56,63 @@ app.get("/testing", async(req, res) => {
     res.send("succesful sending");
 });
 
+//INDEX ROUTE
 app.get("/listings", async(req, res) => {
     const allListings = await Listing.find({});
     console.log("working path");
     res.render("../views/listings/index.ejs", { allListings });
+})
+
+//NEW ROUTE
+app.get("/listings/new", (req, res) => {
+    res.render("../views/listings/new.ejs");
+});
+
+//SHOW ROUTE
+app.get("/listings/:id", async(req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("../views/listings/show.ejs", { listing });
+});
+
+//CREATE ROUTE
+app.post("/listings", async(req, res) => {
+    let {title, description, price, location, country} = req.body;
+    const newListing = new Listing({
+        title,
+        description,
+        price,
+        location,
+        country
+    });
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+//EDIT ROUTE
+app.get("/listings/:id/edit", async(req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("../views/listings/edit.ejs", { listing });
+});
+
+//UPDATE ROUTE
+app.put("/listings/:id", async(req, res) => {
+    let { id } = req.params;
+    let {title, description, price, location, country} = req.body;
+    await Listing.findByIdAndUpdate(id, {
+        title,
+        description,      
+        price,
+        location,
+        country
+    });
+    res.redirect(`/listings/${id}`);
+});
+
+//DELETE ROUTE
+app.delete("/listings/:id", async(req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
 })
