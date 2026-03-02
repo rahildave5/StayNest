9f7b2866c7af586000f2b397387a7e04e5fca4e4
Rahil Dave <153646627+rahildave5@users.noreply.github.com>
2026-03-02 23:00:30 +0530

    Add files via upload

---
+++ listing.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: String,
+    description: String,
+    image: { 
+    type: String,
+    default: "https://blog.3bee.com/en/nature-positive-regenerating-nature-for-the-future/",
+    set: (v) =>
+        v === ""
+            ? "https://blog.3bee.com/en/nature-positive-regenerating-nature-for-the-future/"
+            : v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing; 
