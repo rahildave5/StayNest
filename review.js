f1f8950dc43c679c47ae05a2a6fd8b0e98bbe087
Rahil Dave <153646627+rahildave5@users.noreply.github.com>
2026-03-26 23:11:11 +0530

    Add files via upload

---
+++ review.js
@@ -0,0 +1,22 @@
+const { time } = require('console');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+    comment: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+});
+
+module.exports = mongoose.model('Review', reviewSchema);
